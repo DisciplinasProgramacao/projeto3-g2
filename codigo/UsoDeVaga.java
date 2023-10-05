ce4494bb70b826aec0be6593f6a1e911eebe2a3a
Caroline Teresa <103903445+CarolzTS@users.noreply.github.com>
2023-10-04 21:55:40 -0300

    Update UsoDeVaga.java
    começo de desenvolvimento da classe "UsoDeVaga". Desenvolvimento "public double sair(LocalDateTime saida) ", "public Vaga UsoDeVaga(Vaga vaga, Cliente cliente, LocalDateTime entrada)", e adição de atributos.

--- codigo/UsoDeVaga.java
+++ codigo/UsoDeVaga.java
@@ -1,4 +1,5 @@
-
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class UsoDeVaga {
 
@@ -9,21 +10,39 @@ public class UsoDeVaga {
 	private LocalDateTime entrada;
 	private LocalDateTime saida;
 	private double valorPago;
+	private Cliente cliente;
 
-	public UsoDeVaga(Vaga vaga) {
-		
+	public Vaga UsoDeVaga(Vaga vaga, Cliente cliente, LocalDateTime entrada) {
+		int fila;
+		int numero;
+		String nome;
+		String id;
+		this.vaga = vaga;
+		this.cliente = cliente;
+		this.entrada = entrada;
+		return vaga;
 	}
 
-	public double sair() {
-		
+	public double sair(LocalDateTime saida) {
+		if (saida.isAfter(entrada)) {
+			this.saida = saida;
+			valorPago();
+		} else {
+			System.out.println("Data invalida.");
+		}
+		return valorPago;
 	}
 
-	public boolean ehDoMes(int mes){
-		
+	public boolean ehDoMes(int mes) {
+		// a ser desenvolvido
+		return false;
+
 	}
-	
+
 	public double valorPago() {
-		
+		// a ser desenvolvido
+		return valorPago;
+
 	}
 
 }
