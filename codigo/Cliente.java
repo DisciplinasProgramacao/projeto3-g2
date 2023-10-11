d5f42a3bdb02e12322a357b3d8bdd2b168a72529
Raquel Calazans <raquel.calazans@localiza.com>
2023-10-11 20:21:13 -0300

    Add implementação da classe Cliente

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -7,31 +7,53 @@ public class Cliente {
 	private Veiculo[] veiculos;
 
 	public Cliente(String nome, String id) {
-		
+		this.nome = nome;
+		this.id = id;
 	}
 
 	public void addVeiculo(Veiculo veiculo) {
-		
+		for (int i = 0; i <= veiculos.length; i++) {
+			if (veiculos[i] == null) {
+				veiculos[i] = veiculo;
+			} 
+		}
 	}
 
 	public Veiculo possuiVeiculo(String placa) {
-		
+		Veiculo veiculo = new Veiculo(placa);
+
+		return veiculo;
 	}
 
 	public int totalDeUsos() {
-		
+		return veiculos.length;
 	}
 
 	public double arrecadadoPorVeiculo(String placa) {
-		
+		Veiculo veiculo = new Veiculo(placa);
+		double valorArrecadado = veiculo.totalArrecadado();
+
+		return valorArrecadado;
 	}
 
 	public double arrecadadoTotal() {
-		
+		double total = 0;
+
+		for (int i = 0; i <= veiculos.length; i++) {
+			total += veiculos[i].totalArrecadado();
+		}
+
+		return total;
 	}
 
 	public double arrecadadoNoMes(int mes) {
-		
+		double totalMes = 0;
+
+		for (int i = 0; i <= veiculos.length; i++) {
+			totalMes += veiculos[i].arrecadadoNoMes(mes);
+		}
+
+		return totalMes;
 	}
 
 }
