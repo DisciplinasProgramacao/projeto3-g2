7efd16099f8aeb4f35dd57ccd321a2bfabdad193
Raquel Calazans <raquel.calazans@localiza.com>
2023-10-25 21:37:04 -0300

    Criar teste do estacionamento

---
+++ codigo/EstacionamentoTest.java
@@ -0,0 +1,129 @@
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EstacionamentoTest {
+    private Estacionamento estacionamento;
+	private Cliente[] id;
+	private Vaga[] vagas;
+	private int fileiras; 
+	private int vagasPorFileira; 
+
+    @Before
+    public void setUp() {
+        estacionamento = new Estacionamento("João", fileiras, vagasPorFileira);
+    }
+
+    // public void addVeiculo(Veiculo veiculo, String idCli) {
+	// 	Cliente novoCliente = new Cliente(idCli);
+	// 	novoCliente.addVeiculo(veiculo);
+
+	// 	for(int i = 0; i <= id.length; i++) {
+	// 		if (id[i] == null) {
+	// 			id[i] = novoCliente;
+	// 		} 
+	// 	}
+	// }
+
+	// public void addCliente(Cliente cliente) {
+	// 	for(int i = 0; i <= id.length; i++) {
+	// 		if (id[i] == null) {
+	// 			id[i] = cliente;
+	// 		} 
+	// 	}
+	// }
+
+	// private void gerarVagas() {
+	// 	int count = 0; 
+
+	// 	for (int i = 0; i <= fileiras; i++) {
+	// 		for (int j = 0; j <= vagasPorFileira; j++) {
+	// 			Vaga vaga = new Vaga(i, j);
+
+	// 			vagas[count] = vaga;
+	// 			count++;
+	// 		}
+	// 	}
+	// }
+
+	// public void estacionar(String placa) {
+	// 	for (int i = 0; i <= vagas.length; i++) {
+	// 		if (vagas[i] != null && vagas[i].disponivel()) {
+	// 			Veiculo veiculo = new Veiculo(placa);
+	// 			veiculo.estacionar(vagas[i]);
+	// 		}
+	// 	}
+	// }
+
+	// public double sair(String placa) {
+	// 	double valorTotal = 0;
+	// 	Veiculo veiculo = new Veiculo(placa);
+
+    //     for (int i = 0; i < id.length; i++) {
+    //         if (id[i] != null) {
+	// 			if (id[i].possuiVeiculo(placa) == veiculo) {
+	// 				valorTotal = veiculo.sair();
+	// 			}
+    //         }
+    //     }
+
+    //     return valorTotal;
+	// }
+
+	// public double totalArrecadado() {
+	// 	double total = 0;
+
+	// 	for (int i = 0; i <= id.length; i++) {
+	// 		if (id[i] != null) {
+	// 			total += id[i].arrecadadoTotal();
+	// 		}
+	// 	}
+
+	// 	return total;
+	// }
+
+	// public double arrecadacaoNoMes(int mes) {
+	// 	double totalMes = 0;
+
+	// 	for (int i = 0; i <= id.length; i++) {
+	// 		if (id[i] != null) {
+	// 			totalMes += id[i].arrecadadoNoMes(mes);
+	// 		}
+	// 	}
+
+	// 	return totalMes;
+	// }
+
+	// public double valorMedioPorUso() {
+	// 	double totalDeUsos = 0;
+	// 	double arrecadadoTotal = 0;
+
+	// 	for (int i = 0; i <= id.length; i++) {
+	// 		totalDeUsos += id[i].totalDeUsos();
+	// 		arrecadadoTotal += id[i].arrecadadoTotal();
+	// 	}
+
+	// 	return arrecadadoTotal/totalDeUsos;
+	// }
+
+	// public String top5Clientes(int mes) {
+	// 	double arrayArrecadado[] = {0,0,0,0,0};
+	// 	String topClientes[];
+
+	// 	for (int i = 0; i <= id.length; i++) {
+	// 		double arrecadado = id[i].arrecadadoNoMes(mes);
+	// 		for (int j = 0; j <= 5; j++) {
+	// 			if (arrecadado > arrayArrecadado[j]) {
+	// 				arrayArrecadado[j] = arrecadado;
+	// 				topClientes[j] = id[i].nome;
+	// 			}
+	// 		}
+	// 	}
+
+	// 	return "";
+	// }
+}
